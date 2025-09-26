5b7e5c2ab3d2c7042ae2316c2ce5283890c9bda7
zen3t <josealvess6154@gmail.com>
2025-09-26 18:06:50 -0300

    Troquei so o nome da pasta

--- aula4_parte1/Views.py
+++ aula4_parte1/Views.py
@@ -1,9 +1,13 @@
 """Extencao flask"""
 
+from flask import request
+
 
 def init_app(app):
     @app.route("/")
     def index():
+        print(request.args)
+        print(request.values)
         return "<h1>Ola mundo</h1>"
 
     @app.route("/contato")
